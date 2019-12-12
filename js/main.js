e1e654487d3ebfe20b68afa967a7174b4fc4adef
Mila Bozhevskaya <mi.bozhev@gmail.com>
2019-12-12 12:28:06 +0300

    noScroll

--- js/main.js
+++ js/main.js
@@ -59,9 +59,16 @@ for (let i = 0; i < teamItemLength; i++) {
 const hamburger = document.querySelector('.hamb-menu');
 const first = document.querySelector('.first');
 const firstClose = document.querySelector('.first__close');
+function noScroll() {
+  window.scrollTo(0, 0);
+}
+
 
 hamburger.addEventListener('click', function(event) {
   event.preventDefault();
+
+  window.addEventListener('scroll', noScroll); 
+
   first.style.display = 'flex';
 });
 
@@ -69,9 +76,12 @@ firstClose.addEventListener('click', function(event) {
   event.stopPropagation();
   event.preventDefault();
 
+  window.removeEventListener('scroll', noScroll);
+
   first.style.display = 'none';
+  
   first.addEventListener('click', function(event) {
-    event.preventDefault();
+     event.preventDefault();
   })
 })
 
