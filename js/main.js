56fb132762794a044fa2fb901a862c82ba8eff8e
Mila Bozhevskaya <mi.bozhev@gmail.com>
2019-12-16 02:29:34 +0300

    плавный бесконечный слайдер

--- js/main.js
+++ js/main.js
@@ -101,45 +101,38 @@ burgers.addEventListener('click', function(event) {
 
 
 ////burgers  slider
-const burgerNext = document.querySelector('.burgers__arrow--next');
-const burgerBack = document.querySelector('.burgers__arrow--back');
-const burgersList = document.querySelector(".burgers__list");
+// const burgerNext = document.querySelector('.burgers__arrow--next');
+// const burgerBack = document.querySelector('.burgers__arrow--back');
+// const burgersList = document.querySelector(".burgers__list");
 
-burgerNext.addEventListener("click", function(event) {
-  event.preventDefault();
-  loop('next', event);
-});
+// burgerNext.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loop('next', event);
+// });
 
-burgerBack.addEventListener("click", function(event) {
-  event.preventDefault();
-  loop("back", event);
-});
+// burgerBack.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loop("back", event);
+// });
 
-function loop(direction, event) {
+// function loop(direction, event) {
 
   
-  if(direction == 'next') {
-    burgersList.appendChild(burgersList.firstElementChild);
-  } else {
-    burgersList.insertBefore(burgersList.lastElementChild, burgersList.firstElementChild);
-  }
-}
+//   if(direction == 'next') {
+//     burgersList.appendChild(burgersList.firstElementChild);
+//   } else {
+//     burgersList.insertBefore(burgersList.lastElementChild, burgersList.firstElementChild);
+//   }
+// }
 
 
-///////form
+///////////////////////////////form
 const form = document.querySelector('#orderform');
 const btnSend = document.querySelector('.btn-send');
 
 btnSend.addEventListener('click', function(event) {
   event.preventDefault();
-  // const data = {
-  //     name: form.elements.name.value,
-  //     phone: form.elements.phone.value,
-  //     comment: "",
-  //     to: 'mi.bozhev@gmail.com'
-  //   };
-    // console.log(data);
-
+ 
     const formData = new FormData();
     formData.append('name',  form.elements.name.value);
     formData.append('phone',  form.elements.phone.value);
@@ -175,3 +168,141 @@ function validField(field) {
   field.nextElementSibling.textContent = field.validationMessage;
   return field.checkValidity();
 }
+
+/////////////////////////////////////////////////////////////
+/// плавный бесконечный слайдер
+let item = document.querySelectorAll('.burgers__item');
+const itemLength = item.length;
+// console.log(itemLength);
+const burgers__list = document.querySelector('.burgers__list');
+const rightArrow = document.querySelector(".burgers__arrow--back");
+const leftArrow = document.querySelector(".burgers__arrow--next");
+let slider = [];
+for (let i = 0; i<itemLength; i++) {
+  slider[i] = item[i];
+  // console.log(slider[i]);
+  item[i].remove();
+}
+let step = 0;
+let offset = 0;
+
+function burgerSlider() {
+  let div = document.createElement('li');
+  div = slider[slider.length-1];
+  div.classList.add('burgers__item');
+  div.style.left = -100 + '%';
+  // console.log(div);
+  burgers__list.appendChild(div); 
+  
+  
+  div = slider[step];
+  // div.classList.add('item');
+  div.style.left = offset*100 + '%';
+  // console.log(div);
+  burgers__list.appendChild(div); 
+  div = slider[step+1];
+  // div.classList.add('item');
+  div.style.left = offset*100 + 100 + '%';
+  // console.log(div);
+  burgers__list.appendChild(div); 
+  offset = 1;
+  
+}
+function burgerSliderL() {
+  console.log(step);
+  if (step == (slider.length-1)) {
+    step = 1;
+  } else {
+    if (step == (slider.length-2)) {
+      step = 0;
+    } else {
+      step = (step +2);
+    }
+  }
+  console.log(step);
+  let div = document.createElement('li');
+  div = slider[step];
+  div.classList.add('burgers__item');
+  div.style.left = offset*100 + '%';
+  // console.log(div);
+  burgers__list.appendChild(div); 
+    
+  if (step == 0) {
+    step = (slider.length-1);
+  } else {
+    step = (step - 1);
+  }
+  console.log(step);
+  offset = 1;
+}
+
+function left() {
+  leftArrow.onclick = null;
+  // 
+  let slider2 = document.querySelectorAll('.burgers__item');
+  let offset2 = -1;
+  // console.log(slider2.length);
+  for (let i = 0; i<slider2.length; i++) {
+    slider2[i].style.left = offset2*100 - 100 + '%';
+    offset2 ++;
+  }
+  setTimeout(function() {
+    slider2[0].remove();
+    burgerSliderL();
+    leftArrow.onclick = left;
+  }, 600);
+  
+}
+
+function burgerSliderR() {
+  console.log(step);
+  if (step == 0) {
+    step = (slider.length-2);
+  } else {
+    if (step == 1) {
+      step = (slider.length-1);
+    } else {
+      step = (step -2);
+    }
+  }
+  console.log(step);
+  let offset = -1;
+  let div = document.createElement('li');
+  div = slider[step];
+  div.classList.add('burgers__item');
+  div.style.left = offset*100 + '%';
+  // console.log(div);
+  burgers__list.insertBefore(div, burgers__list.firstElementChild);
+  if (step == (slider.length-1)) {
+      step = 0;
+   } else {
+     step = (step+1);
+   }
+  console.log(step);
+  offset = 1;
+}
+
+function right() {
+  rightArrow.onclick = null;
+  
+  let slider2 = document.querySelectorAll('.burgers__item');
+  let offset2 = (slider2.length-1);
+ 
+  for (let i = (slider2.length-1); i>=0; i--) {
+    slider2[i].style.left = offset2*100 + '%';
+    offset2 --;
+  }
+  setTimeout(function() {
+    slider2[(slider2.length-1)].remove();
+     burgerSliderR();
+    rightArrow.onclick = right;
+  }, 600);
+}
+
+
+burgerSlider();
+step = 0;
+
+leftArrow.onclick = left;
+rightArrow.onclick = right;
+
